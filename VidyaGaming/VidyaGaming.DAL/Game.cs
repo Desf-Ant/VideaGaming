ff71c6690424afe9252e4c434bcff362c6f69adc
ThePetman <79446372+ThePetman@users.noreply.github.com>
2022-11-18 14:59:53 +0100

    class Game v1

--- VidyaGaming/VidyaGaming.DAL/Game.cs
+++ VidyaGaming/VidyaGaming.DAL/Game.cs
@@ -8,6 +8,13 @@ namespace VidyaGaming.DAL
 {
     public class Game
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public float Note { get; set; } 
+        public string Genre { get; set; }       
+        public List<String> Commentaires { get; set; }
         public Game() { 
         
         }
