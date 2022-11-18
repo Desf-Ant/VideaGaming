1ee0664407d1abbe872613c045b44ef45b020c05
ThePetman <79446372+ThePetman@users.noreply.github.com>
2022-11-18 14:25:24 +0100

    re test

--- VidyaGaming/VidyaGaming/Controllers/HomeController.cs
+++ VidyaGaming/VidyaGaming/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using VidyaGaming.Models;
 
 namespace VidyaGaming.Controllers;
-
+//commentaire test
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
